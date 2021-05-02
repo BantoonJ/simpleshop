5f01d62a42edd2ad8b9a7019b3479b3d2efbbf29
Por <por@nuxos.asia>
2021-05-02 17:33:20 +0700

    fixed confirmation get error

--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
              },
              controllers: {
                sessions: 'sessions',
-               registrations: 'registrations'
-             }
+               registrations: 'registrations',
+               confirmations: 'confirmations'
+  }
 
   # devise_for :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
